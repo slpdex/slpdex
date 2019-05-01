7ed96f791dfe85281702c3ab35d03ccce6342eaa
Chipp! <alex@ryberg.com>
2019-05-01 23:15:15 +0200

    feat(landing page): add tons of landing page design & markup

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
 import { SharedModule } from '../shared/shared.module';
+import { QuickOrderComponent } from './quick-order/quick-order.component';
 
 @NgModule({
-  declarations: [LandingComponent],
+  declarations: [LandingComponent, QuickOrderComponent],
   imports: [CommonModule, LandingRoutingModule, SharedModule],
 })
 export class LandingModule {}
---
+++ src/app/landing/quick-order/quick-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quick-order',
+  templateUrl: './quick-order.component.html',
+  styleUrls: ['./quick-order.component.scss']
+})
+export class QuickOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
