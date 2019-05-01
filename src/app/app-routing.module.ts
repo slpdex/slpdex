53a31031c7f109eb2d1dc13a256f00bdba2a5746
Chipp! <alex@ryberg.com>
2019-05-02 00:17:21 +0200

    feat(preloading): enable module preloading for lazy loaded routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { SLPRoutes } from './shared/slp-routes';
 
 const routes: Routes = [
@@ -14,7 +14,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
