483b58b7cb9d6e57fda6f17bac3b6dfdefde2b54
Chipp! <alex@ryberg.com>
2019-05-05 21:15:12 +0200

    feat(landing-content): update styling

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from '../shared/shared.module';
 import { LandingComponent } from './landing.component';
 import { QuickOrderComponent } from './quick-order/quick-order.component';
 import { LandingContentComponent } from './landing-content/landing-content.component';
+import { LandingRecentComponent } from './landing-recent/landing-recent.component';
 
 @NgModule({
-  declarations: [LandingComponent, QuickOrderComponent, LandingContentComponent],
+  declarations: [LandingComponent, QuickOrderComponent, LandingContentComponent, LandingRecentComponent],
   imports: [CommonModule, SharedModule, RouterModule],
 })
 export class LandingModule {}
