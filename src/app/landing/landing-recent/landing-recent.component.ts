2ff82eb00f875a1c479c92b572a9f2353a568bd5
Chipp! <alex@ryberg.com>
2019-05-05 21:20:06 +0200

    feat(landing-recent): add styling & mock markup

---
+++ src/app/landing/landing-recent/landing-recent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-recent',
+  templateUrl: './landing-recent.component.html',
+  styleUrls: ['./landing-recent.component.scss']
+})
+export class LandingRecentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
