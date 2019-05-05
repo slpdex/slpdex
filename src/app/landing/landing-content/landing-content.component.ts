0b57b70b442e3ade0fca8745dc0fad82778795bf
Chipp! <alex@ryberg.com>
2019-05-05 21:01:42 +0200

    feat(icons & landing-content): add new svg icons & update landing content styling

---
+++ src/app/landing/landing-content/landing-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-content',
+  templateUrl: './landing-content.component.html',
+  styleUrls: ['./landing-content.component.scss']
+})
+export class LandingContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { LandingComponent } from './landing.component';
 import { QuickOrderComponent } from './quick-order/quick-order.component';
+import { LandingContentComponent } from './landing-content/landing-content.component';
 
 @NgModule({
-  declarations: [LandingComponent, QuickOrderComponent],
+  declarations: [LandingComponent, QuickOrderComponent, LandingContentComponent],
   imports: [CommonModule, SharedModule, RouterModule],
 })
 export class LandingModule {}
