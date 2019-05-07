b8cec9fd136d52aafaae412e2ac16fc1ad964ec9
Alex Ryberg <alex.ryberg@hotmail.com>
2019-05-07 14:27:16 +0200

    feat(clean): add clean prod script

---
+++ clean-prod.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const dir = './prod';
+const dirAssets = `${dir}/assets`;
+
+const ignore = ['CNAME', '.git'];
+
+const removeFile = (file, filePath) => {
+  const validFile = !ignore.find(x => x === file);
+
+  if (validFile) {
+    fs.unlink(`${filePath}/${file}`);
+  }
+};
+
+fs.readdir(dir, (err, files) => {
+  if (err) {
+    return;
+  }
+
+  if (!files || !files.length) {
+    return;
+  }
+
+  files.forEach(file => removeFile(file, dir));
+});
+
+fs.readdir(dirAssets, (err, files) => {
+  if (err) {
+    return;
+  }
+
+  if (!files || !files.length) {
+    return;
+  }
+
+  files.forEach(file => removeFile(file, dirAssets));
+});
