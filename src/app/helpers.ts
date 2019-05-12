124d811d963067c0700e41518fcaa4b1a1bdff54
Chipp! <alex@ryberg.com>
2019-05-12 19:46:47 +0200

    fix(helpers): incorrect bch & sats conversion

--- src/app/helpers.ts
+++ src/app/helpers.ts
@@ -1,9 +1,9 @@
 import BigNumber from 'bignumber.js';
 
 export const convertSatsToBch = (sats: BigNumber) => {
-  return sats.dividedBy(10000000).decimalPlaces(8);
+  return sats.dividedBy(100000000).decimalPlaces(8);
 };
 
 export const convertBchToSats = (sats: BigNumber) => {
-  return sats.times(10000000).decimalPlaces(8);
+  return sats.times(100000000).decimalPlaces(8);
 };
