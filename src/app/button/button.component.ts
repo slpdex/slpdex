cbfb6ad77a4486dc453389f6bbc5f2ff4fdfa2d4
Chipp! <alex@ryberg.com>
2019-05-12 15:48:32 +0200

    feat(button): add button component

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
