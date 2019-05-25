793e4851e2403670bdd18d0f854c4ddbef79fd64
Chipp! <alex@ryberg.com>
2019-05-25 22:44:34 +0200

    refactor(tokens-details): formatting

--- src/app/module/tokens/tokens-details/tokens-details-orderbook/tokens-details-orderbook.component.ts
+++ src/app/module/tokens/tokens-details/tokens-details-orderbook/tokens-details-orderbook.component.ts
@@ -5,9 +5,14 @@ import {
   ViewChild,
   ElementRef,
   AfterViewInit,
+  OnDestroy,
 } from '@angular/core';
 
 import SimpleBar from 'simplebar';
+import { ActivatedRoute } from '@angular/router';
+import { take, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { CashContractsService } from '../../../../cash-contracts.service';
 
 @Component({
   selector: 'app-tokens-details-orderbook',
@@ -15,14 +20,22 @@ import SimpleBar from 'simplebar';
   styleUrls: ['./tokens-details-orderbook.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TokensDetailsOrderbookComponent implements OnInit, AfterViewInit {
+export class TokensDetailsOrderbookComponent
+  implements OnInit, OnDestroy, AfterViewInit {
   tests = Array(30).fill(false);
 
-  @ViewChild('list') list: ElementRef<HTMLElement>;
+  private destroy$ = new Subject();
+
+  @ViewChild('list', { static: false }) list: ElementRef<HTMLElement>;
 
   constructor() {}
 
   ngOnInit() {}
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
+  }
 
   ngAfterViewInit() {
     const simpleBar = new SimpleBar(this.list.nativeElement);
