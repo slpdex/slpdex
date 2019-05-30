19e5f94c4ed6691242df71c28fe7cdbe193084ee
Chipp! <alex@ryberg.com>
2019-05-30 21:08:26 +0200

    refactor(endpoints): remove unused getAllTokens query

--- src/app/endpoints.service.ts
+++ src/app/endpoints.service.ts
@@ -1,22 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { AllTokens, allTokensQuery } from './queries/allTokensQuery';
+import { bchUsdQuery, CryptoNator } from './queries/bchUsdQuery';
 import {
   TimeSinceLastBlock,
   timeSinceLastBlockQuery,
 } from './queries/timeSinceLastBlockQuery';
 import { TokenDetails, tokenDetailsQuery } from './queries/tokenDetailsQuery';
-import { bchUsdQuery, CryptoNator } from './queries/bchUsdQuery';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EndpointsService {
   constructor(private httpClient: HttpClient) {}
-
-  getAllTokens = () => {
-    return this.httpClient.get<AllTokens>(allTokensQuery());
-  };
 
   getTokenDetails = (symbol: string) => {
     return this.httpClient.get<TokenDetails>(tokenDetailsQuery(symbol));
