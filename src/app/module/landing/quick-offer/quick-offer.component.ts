6eaa9ba78e82d814fad97ae81c9d242b48ea8b13
Chipp! <alex@ryberg.com>
2019-06-02 18:13:04 +0200

    feat(quick-offer): design remake & add awesome animations

--- src/app/module/landing/quick-offer/quick-offer.component.ts
+++ src/app/module/landing/quick-offer/quick-offer.component.ts
@@ -1,13 +1,17 @@
 import {
+  ChangeDetectionStrategy,
   Component,
   ElementRef,
+  OnDestroy,
   OnInit,
   ViewChild,
-  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  AfterViewInit,
 } from '@angular/core';
-import { Router } from '@angular/router';
-import { AnimeAnimParams } from 'animejs';
-import anime from 'animejs/lib/anime.es.js';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { MarketService } from '../../../market.service';
+import { TokenOverview } from 'slpdex-market';
 import { SLPRoutes } from '../../../slp-routes';
 
 @Component({
@@ -16,37 +20,122 @@ import { SLPRoutes } from '../../../slp-routes';
   styleUrls: ['./quick-offer.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QuickOfferComponent implements OnInit {
-  animateButton = false;
+export class QuickOfferComponent implements OnInit, OnDestroy, AfterViewInit {
+  selectedTokenLeft: TokenOverview = {} as TokenOverview;
+  selectedTokenRight: TokenOverview = {} as TokenOverview;
 
-  @ViewChild('button', { static: false }) button: ElementRef<HTMLElement>;
-  @ViewChild('dummy', { static: false }) dummy: ElementRef<HTMLElement>;
+  slpRoutes = { ...SLPRoutes };
 
-  constructor(private router: Router) {}
+  private destroy$ = new Subject();
+  private tokenOverview: TokenOverview[];
+  private animationStarted = false;
 
-  ngOnInit() {}
+  private selectedTokenIndex = 0;
 
-  createoffer = () => {
-    const element = this.button.nativeElement;
-    const dummyElement = this.dummy.nativeElement;
+  private animateLeftClass = 'quick-offer__animate--left';
+  private animateRightClass = 'quick-offer__animate--right';
+  private hideClass = 'quick-offer__hide';
 
-    dummyElement.style.display = 'block';
-    dummyElement.style.opacity = '1';
+  private slideShowInterval: number;
 
-    anime({
-      targets: dummyElement,
-      top: [element.offsetTop - window.scrollY, 0],
-      left: [element.offsetLeft, 0],
-      width: [element.clientWidth, window.innerWidth],
-      height: [element.clientHeight, window.innerHeight],
-      bofferRadius: [10, 0],
-      background: ['#5ebd6d', '#82cc8e'],
-      easing: 'easeInQuad',
-      duration: 400,
-    } as AnimeAnimParams);
+  @ViewChild('tokenBoxLeft', { static: false }) tokenBoxLeft: ElementRef<
+    HTMLElement
+  >;
 
-    setTimeout(() => {
-      this.router.navigateByUrl(SLPRoutes.offer);
-    }, 600);
+  @ViewChild('tokenBoxRight', { static: false }) tokenBoxRight: ElementRef<
+    HTMLElement
+  >;
+
+  @ViewChild('bchBox', { static: false }) bchBox: ElementRef<HTMLElement>;
+
+  constructor(
+    private marketService: MarketService,
+    private changeDetectorRef: ChangeDetectorRef,
+  ) {}
+
+  ngOnInit() {
+    this.marketService.loadMarketOverview('marketCapSatoshis', false);
+  }
+
+  ngOnDestroy() {
+    this.stopSlideShow();
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
+  }
+
+  ngAfterViewInit() {
+    setTimeout(() => this.listenMarketOverview(), 1000);
+  }
+
+  stopSlideShow = () => {
+    window.clearInterval(this.slideShowInterval);
+  };
+
+  private listenMarketOverview = () => {
+    this.marketService.marketOverview
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(overview => {
+        if (!overview.length) {
+          return;
+        }
+
+        this.tokenOverview = overview;
+
+        if (!this.animationStarted) {
+          this.animationStarted = true;
+          this.selectedTokenLeft = overview[this.selectedTokenIndex];
+          this.changeDetectorRef.markForCheck();
+
+          this.startSlideShow();
+          this.firstAnimation();
+        }
+      });
+  };
+
+  startSlideShow = () => {
+    this.stopSlideShow();
+
+    this.slideShowInterval = window.setInterval(() => {
+      this.setTokens();
+      window.setTimeout(() => this.slideBch(), 1000);
+    }, 4000);
+  };
+
+  private firstAnimation = () => {
+    this.bchBox.nativeElement.classList.add(this.animateRightClass);
+    this.tokenBoxLeft.nativeElement.classList.add(this.animateLeftClass);
+
+    setTimeout(
+      () => this.tokenBoxRight.nativeElement.classList.remove(this.hideClass),
+      1000,
+    );
+  };
+
+  private slideBch = () => {
+    const element = this.bchBox.nativeElement;
+
+    if (element.classList.contains(this.animateLeftClass)) {
+      element.classList.remove(this.animateLeftClass);
+      element.classList.add(this.animateRightClass);
+    } else {
+      element.classList.remove(this.animateRightClass);
+      element.classList.add(this.animateLeftClass);
+    }
+  };
+
+  private setTokens = () => {
+    this.selectedTokenIndex++;
+
+    if (this.selectedTokenIndex >= this.tokenOverview.length) {
+      this.selectedTokenIndex = 0;
+    }
+
+    if (this.selectedTokenIndex % 2 === 0) {
+      this.selectedTokenLeft = this.tokenOverview[this.selectedTokenIndex];
+    } else {
+      this.selectedTokenRight = this.tokenOverview[this.selectedTokenIndex];
+    }
+
+    this.changeDetectorRef.markForCheck();
   };
 }
