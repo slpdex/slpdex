3d103c56a7b345f5912825e292443f64b1448d08
Chipp! <alex@ryberg.com>
2019-06-11 02:00:14 +0200

    feat(coin-card): add noopener

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,6 +18,26 @@
  * BROWSER POLYFILLS
  */
 
+/** IE9, IE10 and IE11 requires all of the following polyfills. */
+// import 'core-js/es6/symbol';
+// import 'core-js/es6/object';
+// import 'core-js/es6/function';
+// import 'core-js/es6/parse-int';
+// import 'core-js/es6/parse-float';
+// import 'core-js/es6/number';
+// import 'core-js/es6/math';
+// import 'core-js/es6/string';
+// import 'core-js/es6/date';
+// import 'core-js/es6/array';
+// import 'core-js/es6/regexp';
+// import 'core-js/es6/map';
+// import 'core-js/es6/weak-map';
+// import 'core-js/es6/set';
+// import 'core-js/es6/reflect';
+
+// import 'core-js/es7/array';
+// import 'core-js/es7/reflect';
+
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
@@ -55,8 +75,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
