aa3114df78b94e80be482ee86dd54b9d588b033f
Chipp! <alex@ryberg.com>
2019-06-27 20:48:44 +0200

    feat(sentry): log errors & only show reportDialog in prod

--- src/app/sentry-error-handler.service.ts
+++ src/app/sentry-error-handler.service.ts
@@ -1,11 +1,17 @@
 import { ErrorHandler, Injectable } from '@angular/core';
 import * as Sentry from '@sentry/browser';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class SentryErrorHandler implements ErrorHandler {
   constructor() {}
   handleError(error) {
+    console.error(error);
+
     const eventId = Sentry.captureException(error.originalError || error);
-    Sentry.showReportDialog({ eventId });
+
+    if (environment.production) {
+      Sentry.showReportDialog({ eventId });
+    }
   }
 }
