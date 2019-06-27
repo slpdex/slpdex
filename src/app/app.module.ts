44c0e717e902aa446f68fb1e80ac60320789d742
Alex Ryberg <alex.ryberg@hotmail.com>
2019-06-27 10:45:00 +0200

    feat(sentry): add sentry error tracking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import * as Sentry from '@sentry/browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './header/header.module';
 import { IconModule } from './icon/icon.module';
+import { SentryErrorHandler } from './sentry-error-handler.service';
 import { SvgsComponent } from './svgs/svgs.component';
+
+Sentry.init({
+  dsn: 'https://157b399a3a0746f1a6fd150b04c6ebb6@sentry.io/1491592',
+});
 
 @NgModule({
   declarations: [AppComponent, SvgsComponent],
@@ -21,7 +27,7 @@ import { SvgsComponent } from './svgs/svgs.component';
     //   enabled: environment.production,
     // }),
   ],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/sentry-error-handler.service.ts
@@ -0,0 +1,11 @@
+import { ErrorHandler, Injectable } from '@angular/core';
+import * as Sentry from '@sentry/browser';
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  constructor() {}
+  handleError(error) {
+    const eventId = Sentry.captureException(error.originalError || error);
+    Sentry.showReportDialog({ eventId });
+  }
+}
