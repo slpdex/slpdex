8e026643f759fb6ed01826eaf2cf8f2641263a5c
Chipp! <alex@ryberg.com>
2019-06-30 15:32:59 +0200

    feat(sentry): disable reportDialog

--- src/app/sentry-error-handler.service.ts
+++ src/app/sentry-error-handler.service.ts
@@ -1,17 +1,11 @@
 import { ErrorHandler, Injectable } from '@angular/core';
 import * as Sentry from '@sentry/browser';
-import { environment } from '../environments/environment';
 
 @Injectable()
 export class SentryErrorHandler implements ErrorHandler {
   constructor() {}
   handleError(error) {
-    console.error(error);
-
     const eventId = Sentry.captureException(error.originalError || error);
-
-    if (environment.production) {
-      Sentry.showReportDialog({ eventId });
-    }
+    // Sentry.showReportDialog({ eventId });
   }
 }
