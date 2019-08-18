13175f3344582dd3ff96a2b19da3afd74ff9a5f8
Chipp! <alex@ryberg.com>
2019-08-18 14:42:51 +0200

    feat(app): startup logic for socket connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/core';
 import { CashContractsService } from './cash-contracts.service';
 import { SLPRoutes } from './slp-routes';
+import { SlpdexDBApiService } from './slpdexdb-api.service';
 
 @Component({
   selector: 'app-root',
@@ -18,10 +19,16 @@ export class AppComponent implements AfterViewInit {
 
   constructor(
     private cashContractsService: CashContractsService, // private swUpdate: SwUpdate,
+    private slpdexDBApiService: SlpdexDBApiService,
   ) {}
 
   ngAfterViewInit() {
+    this.cashContractsService.publicAddress$.subscribe(publicAddress =>
+      this.slpdexDBApiService.init(publicAddress),
+    );
+
     this.cashContractsService.init();
+
     // if (this.swUpdate.activated) {
     //   this.swUpdate.available.subscribe(async () => {
     //     await this.swUpdate.activateUpdate();
